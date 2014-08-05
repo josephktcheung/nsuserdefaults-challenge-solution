88817c6223a70e641871d8641dad24cc7418afc4
josephktcheung <josephktcheung@gmail.com>
2014-08-05 17:10:34 +0800

    Add CreateAccountViewController and SignInViewController class

---
+++ NSUserDefaults, Segues and Protocols Challenge Solution/CreateAccountViewController.m
@@ -0,0 +1,37 @@
+//
+//  CreateAccountViewController.m
+//  NSUserDefaults, Segues and Protocols Challenge Solution
+//
+//  Created by Joseph Cheung on 5/8/14.
+//  Copyright (c) 2014 Reque.st. All rights reserved.
+//
+
+#import "CreateAccountViewController.h"
+
+@interface CreateAccountViewController ()
+
+@end
+
+@implementation CreateAccountViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ NSUserDefaults, Segues and Protocols Challenge Solution/SignInViewController.h
@@ -0,0 +1,13 @@
+//
+//  SignInViewController.h
+//  NSUserDefaults, Segues and Protocols Challenge Solution
+//
+//  Created by Joseph Cheung on 5/8/14.
+//  Copyright (c) 2014 Reque.st. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SignInViewController : UIViewController
+
+@end
