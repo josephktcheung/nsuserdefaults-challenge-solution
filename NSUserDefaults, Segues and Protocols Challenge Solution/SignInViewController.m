da4e5c755479e3af46fe18c51dc20e240b25ccec
josephktcheung <josephktcheung@gmail.com>
2014-08-05 17:34:46 +0800

    Show coressponding view controllers when create account / login button pressed in signin view controller

--- NSUserDefaults, Segues and Protocols Challenge Solution/SignInViewController.m
+++ NSUserDefaults, Segues and Protocols Challenge Solution/SignInViewController.m
@@ -24,19 +24,23 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
+
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
-}
-*/
-
-- (IBAction)loginButtonPressed:(UIButton *)sender {
+    
 }
 
-- (IBAction)createAccountBarButtonItemPressed:(UIBarButtonItem *)sender {
+- (IBAction)loginButtonPressed:(UIButton *)sender
+{
+    [self performSegueWithIdentifier:@"toViewControllerSegue" sender:sender];
+}
+
+- (IBAction)createAccountBarButtonItemPressed:(UIBarButtonItem *)sender
+{
+    [self performSegueWithIdentifier:@"toCreateAccountViewControllerSegue" sender:sender];
 }
 @end
